deaba8bf1bbed93c7e1bd3cf2c2dd196cd7ff6c3
OsaidB <100098496+OsaidB@users.noreply.github.com>
2024-11-02 23:43:51 +0200

    console.log

--- Services/ClientService.js
+++ Services/ClientService.js
@@ -19,6 +19,9 @@ const ClientService = {
     getAllClients: async () => {
         try {
             const response = await axios.get(API_URL);
+            console.log(response);
+            console.log("response.data",response.data);
+
             return response.data;
         } catch (error) {
             console.error('Error fetching clients:', error);
