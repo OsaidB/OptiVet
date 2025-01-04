27f8a862b080ca79209b3abc22c679632fe50465
OsaidB <100098496+OsaidB@users.noreply.github.com>
2025-01-04 23:08:23 +0200

    integrated with deployed version of backend

--- Services/config.js
+++ Services/config.js
@@ -4,7 +4,9 @@ const BASE_URLS = {
     OsaidPC: "http://192.168.1.51:8080", // Osaid's PC base URL
     OsaidLaptop: "http://192.168.137.1:8080", // Osaid's laptop base URL
     OsaidLaptopHome: "http://192.168.0.105:8080", // Osaid's laptop base URL
+    OsaidLaptopJawaker: "http://192.168.2.15:8080", // Osaid's laptop base URL
     local: "http://localhost:8080", // Osaid's laptop base URL
+    server: "https://optivet-backend.onrender.com", // Osaid's laptop base URL
 
     MurrarLaptopHome: "http://192.168.56.1:8080", // Murrar's laptop base URL
     //////////////////////////////////////////////////////////////////////////////////////
@@ -21,7 +23,8 @@ const BASE_URLS = {
 };
 
 // Choose the base URL you want to use
-const USED_BASE_URL = BASE_URLS.SwitiLaptop; // Change this to select a different base URL
+const USED_BASE_URL = BASE_URLS.server; // Change this to select a different base URL
+// const USED_BASE_URL = BASE_URLS.SwitiLaptop; // Change this to select a different base URL
 
 const baseURL = {
     USED_BASE_URL,
