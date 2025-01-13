3d5796013a0a23aafb7093f22f59f56900a50be8
mohammad <54774286+kevinhamood@users.noreply.github.com>
2025-01-13 05:52:06 +0200

    PetsForAdoption

---
+++ Services/PetForAdoptionService.js
@@ -0,0 +1,243 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import baseURL from './config'; // Adjust the path as necessary
+const BASE_URL = `${baseURL.USED_BASE_URL}/api/petsForAdoption`;
+const BASE_URL_IMG = `${baseURL.USED_BASE_URL}/api/pets`;
+
+
+
+const PetForAdoptionService = {
+
+    
+    getToken: async () => {
+        return await AsyncStorage.getItem('authToken');
+    },
+
+
+
+    createPetForAdoption: async (petForAdoptionData) => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const response = await axios.post(`${BASE_URL}`, petForAdoptionData, {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error creating pet for adoption:', error);
+            throw error;
+        }
+    },
+
+
+
+
+    uploadPetForAdoptionImages: async (imageUri) => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const formData = new FormData();
+
+            if (Platform.OS === 'web') {
+                const response = await fetch(imageUri);
+                console.log(response);
+                const blob = await response.blob();
+                formData.append('image', blob, 'petForAdoptionImage.jpg');
+            } else {
+                formData.append('image', {
+                    uri: imageUri,
+                    name: 'petForAdoptionImage.jpg',
+                    type: 'image/jpeg',
+                });
+            }
+
+            const response = await axios.post(`${BASE_URL}/uploadImage`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'X-Auth-Token': token,
+                },
+            });
+console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error uploading pet for adoption image:', error);
+            throw error;
+        }
+    },
+
+
+
+
+
+
+    getpetsForAdoption: async () => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const response = await axios.get(BASE_URL, {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching pets for adoption:', error);
+            throw error;
+        }
+    },
+
+
+    servePetForAdoptionImage: async (name) => {
+
+
+        try {
+
+            const response = await axios.get(`${BASE_URL}/uploads/${name}`, {
+                headers: {
+
+                    'X-Auth-Token': token,
+
+                },
+
+
+
+
+            },
+                { responseType: 'blob' }
+            );
+
+
+            return URL.createObjectURL(response.data);
+
+        } catch (uploadingError) {
+            console.log('Error serving pet for adoption image', uploadingError);
+            throw uploadingError;
+
+        }
+    },
+
+
+
+
+    deletePetForAdoption: async (petForAdoptionId) => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const response = await axios.delete(`${BASE_URL}/${petForAdoptionId}`, {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(`Error deleting pet for adoption with ID: ${petForAdoptionId}`, error);
+            throw error;
+        }
+    },
+
+
+
+
+
+
+    handleUpload: async (imageUri) => {
+
+        try {
+
+            const token = await PetForAdoptionService.getToken();
+            const formData = new FormData();
+
+            if (Platform.OS === 'web') {
+                const response = await fetch(BASE_URL_IMG + imageUri, {
+                    method: "GET",
+                    headers: {
+                        'X-Auth-Token': token,
+                    },
+                });
+                //console.log(response);
+                const blob = await response.blob();
+                formData.append('image', blob, 'petForAdoptionImage.jpg');
+                //console.log(formData);
+            } else {
+                formData.append('image', {
+                    uri: imageUri,
+                    name: 'petForAdoptionImage.jpg',
+                    type: 'image/jpeg',
+                });
+            }
+
+            const response = await axios.post(`${BASE_URL}/uploadImage`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'X-Auth-Token': token,
+                },
+            });
+
+            return response.data;
+        } catch (error) {
+            console.error('Error uploading pet for adoption image:', error);
+            throw error;
+        }
+
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+    getPetForAdoptionById: async (petForAdoptionId) => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const response = await axios.get(`${BASE_URL}/${petForAdoptionId}`, {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(`Error fetching pet for adoption with ID: ${petForAdoptionId}`, error);
+            throw error;
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+    updatePetForAdoptionById: async (petForAdoptionId, petForAdoptionData) => {
+        try {
+            const token = await PetForAdoptionService.getToken();
+            const response = await axios.put(`${BASE_URL}/${petForAdoptionId}`, petForAdoptionData, {
+                headers: {
+                    'X-Auth-Token': token,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error(`Error updating pet for adoption with ID: ${petForAdoptionId}`, error);
+            throw error;
+        }
+    },
+
+
+
+};
+
+export default PetForAdoptionService;
--- app/SecretaryStack/index.jsx
+++ app/SecretaryStack/index.jsx
@@ -1,16 +1,168 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import {
+    View, Text, Image, TouchableOpacity,
+    StyleSheet, Alert, ImageBackground, TextInput, ScrollView, Platform
+} from "react-native";
+import { useRouter, Link, useLocalSearchParams, usePathname } from 'expo-router';
+import * as ImagePicker from "expo-image-picker";
+import { Dimensions } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Ionicons } from '@expo/vector-icons';
+import { Icon } from "@rneui/themed";
+import { createFilter } from "react-native-search-filter";
+import ProductService from "../../Services/ProductService";
+import { useNavigation } from "expo-router";
+import baseURL from '../../Services/config'; // Adjust the path as necessary
+//import { useRouter, useLocalSearchParams, Link } from 'expo-router'; // Import useLocalSearchParams
+//import { useRouter, useLocalSearchParams } from 'expo-router'; // Import useLocalSearchParams
+
+
+
+const BASE_URL = `${baseURL.USED_BASE_URL}/api/products`;
+const BASE_URL_IMAGES = `${baseURL.USED_BASE_URL}/api/pets`;
+
+
+
 // import { createStackNavigator } from '@react-navigation/stack';
 // import ManagerScheduleScreen from '../../app/ManagerStack/ManagerScheduleScreen';
 // import ManagerNotesScreen from '../../app/ManagerStack/ManagerNotesScreen';
 // import ManagerUserManagementScreen from '../../app/ManagerStack/ManagerUserManagementScreen';
 // import ManagerReportsScreen from '../../app/ManagerStack/ManagerReportsScreen';
-import {Text} from "react-native";
+
 
 // const Stack = createStackNavigator();
 
 const SecretaryStack = () => {
+
+
+    const navigation = useNavigation();
+    const [pets, setPets] = useState([]);
+
     return (
-        <Text>Secretary Dashboard</Text>
+
+
+
+
+        <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+            <View style={{ alignSelf: 'center' }}><Text style={{ fontSize: 40 }}>Pets for adoption</Text></View>
+
+
+
+            <ScrollView
+                contentContainerStyle={{ flexDirection: 'row', flexWrap: 'wrap' }} style={{ alignSelf: 'center', width: '90%', marginVertical: 10 }}>
+
+
+
+                {pets.map((item) => {
+                    return (
+
+                        <View key={item.id} style={{ width: widthRatio, backgroundColor: '#133945', height: 300, margin: 20, justifyContent: 'space-between', alignItems: 'center', borderRadius: 15 }} SearchProduct={SearchProduct} setSearchText={setSearchText}>
+
+                            <View style={{ width: '90%', height: '50%', marginTop: 5 }}>
+                                <Image source={{ uri: `${BASE_URL_IMAGES}${item.productImageUrl}` }} style={{ width: '100%', height: '100%' }} resizeMode="contain"></Image>
+                            </View>
+
+
+                            <Text style={{ fontSize: 20, color: 'white' }}>{item.name}</Text>
+
+
+
+
+                            <Text style={{ fontSize: 20, color: 'white' }}>{item.price + '₪'}</Text>
+
+
+
+
+                            <Text style={{ fontSize: 20, color: '#dddd', fontFamily: 'bold' }}>{item.productCategory}</Text>
+
+
+                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '30%', height: '30%' }}>
+
+
+                                <Link href={{ pathname: '../ManagerStack/UpdateProduct', params: { productId } }} onPress={() => { setProductId(item.id) }} asChild>
+                                    <TouchableOpacity style={{ alignItems: 'center', height: '60%', width: '100%', backgroundColor: 'white', borderRadius: '100%', marginBottom: 20, marginHorizontal: 10 }}>
+
+                                        <Image source={require('../../assets/images/pencil (2).png')} style={{ flex: 1, width: 30, height: 30 }} resizeMode='contain'></Image>
+
+                                    </TouchableOpacity>
+                                </Link>
+
+
+
+                                <TouchableOpacity style={{ alignItems: 'center', height: '60%', width: '100%', backgroundColor: 'white', borderRadius: '100%', marginBottom: 20, marginHorizontal: 10 }} onPress={() => deleteProductHandle(item.id)}>
+
+                                    <Image source={require('../../assets/images/trash.png')} style={{ flex: 1, width: 30, height: 30 }} resizeMode='contain'></Image>
+
+                                </TouchableOpacity>
+
+
+
+                            </View>
+                        </View>
+
+
+                    )
+                })}
+
+
+
+            </ScrollView>
+
+
+
+
+
+
+
+
+            <Link href={{ pathname: "/SecretaryStack/PetsForAdoption" }} asChild>
+                <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', alignSelf: 'center', backgroundColor: '#133945', borderRadius: 18, height: 50, width: '50%', marginVertical: 10 }}>
+                    <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>Add New Pet</Text>
+                </TouchableOpacity>
+            </Link>
+
+        </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // <Stack.Navigator>
         //     <Stack.Screen
         //         name="Schedule"
